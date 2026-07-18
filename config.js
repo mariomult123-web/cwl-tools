4339b83b0b2b4cce2463ad187347802487eadabf
mariomult123-web <mariomult123@gmail.com>
2026-07-18 10:17:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 // Client ID OAuth ottenuto da Google Cloud Console (istruzioni in Auth.gs)
-const CLIENT_ID = 'INCOLLA_QUI_IL_TUO_CLIENT_ID.apps.googleusercontent.com';
+const CLIENT_ID = '958456533323-ftsabmvqiquav4pku0n08q53v0sk8cum.apps.googleusercontent.com';
 
 // URL della tua Web App Apps Script (Distribuisci > Nuova distribuzione > App web)
-const APPS_SCRIPT_URL = 'INCOLLA_QUI_URL_DEL_TUO_APPS_SCRIPT/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzZV7txQc2BWQXrkBmW58DbIKP_gX0YUh5Hmxf-w3UKh_Rv8rx--3ogTwjkh1AGUBs-Lg/exec';
